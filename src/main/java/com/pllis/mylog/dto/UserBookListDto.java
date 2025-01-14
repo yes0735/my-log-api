358f6ae3b3e9cf03a073c7b16585baa8d4b1e850
yeji2 <yejilee472@gmail.com>
2025-01-14 22:31:20 +0900

    25.01.14 UserBookListDto 수정

--- src/main/java/com/pllis/mylog/dto/UserBookListDto.java
+++ src/main/java/com/pllis/mylog/dto/UserBookListDto.java
@@ -5,45 +5,24 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class UserBookListDto {
-    // USER 테이블 필드
-    private int userNo;  // userNo 타입을 int로 변경
+    // USER 테이블 관련 필드
+    private Integer userNo;
     private String userMail;
     private String userNickname;
 
-    // MY_BOOK 테이블 필드
-    private int myBookNo;
+    // MY_BOOK 테이블 관련 필드
+    private Integer myBookNo;
     private String bookTitle;
     private String bookImageLink;
     private String readStatus;
     private Integer totalPage;
     private Integer readPage;
     private Integer scope;
-    private Date registrationDatetime;
-    private Date updateDatetime;
-
-    // 생성자
-    public UserBookListDto(Integer userNo, String userMail, String userNickname,
-                           Integer myBookNo, String bookTitle, String bookImageLink,
-                           String readStatus, Integer totalPage, Integer readPage,
-                           Integer scope, Date registrationDatetime,
-                           Date updateDatetime) {
-        this.userNo = userNo;
-        this.userMail = userMail;
-        this.userNickname = userNickname;
-        this.myBookNo = myBookNo;
-        this.bookTitle = bookTitle;
-        this.bookImageLink = bookImageLink;
-        this.readStatus = readStatus;
-        this.totalPage = totalPage;
-        this.readPage = readPage;
-        this.scope = scope;
-        this.registrationDatetime = registrationDatetime;
-        this.updateDatetime = updateDatetime;
-    }
+    private LocalDateTime registrationDatetime;
+    private LocalDateTime updateDatetime;
 }
