9f5369991c609398fa5b03f7be8eb8e19428f97c
yeji2 <yejilee472@gmail.com>
2025-07-07 20:33:53 +0900

    doc:주석 변경

--- src/main/java/com/pllis/mylog/controller/BookController.java
+++ src/main/java/com/pllis/mylog/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.pllis.mylog.controller;
 
+import com.pllis.mylog.common.exception.UnauthenticatedException;
 import com.pllis.mylog.common.handler.ApiResponseHandler;
 import com.pllis.mylog.dto.UserBookListDto;
 import com.pllis.mylog.service.BookService;
@@ -28,12 +29,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BookController extends BaseController{
     private final BookService bookService;
-    
+
     /**
-     * 등록된 책 리스트
+     * 등록된 책 리스트를 페이징해 조회
+     * 기본 페이지 사이즈는 12이며, 등록일(registrationDatetime)기준으로 정렬
+     *
+     * @param pageable  pageable 페이징 정보
+     * @param readStatus 독서 상태 필터(예:reading,finished 등)
+     * @param sortBy 정렬 기준(예:registrationDatetime,scope)
+     * @param collectionType 소장 유형 필터(예:ebook,paper_book등)
      * @return 나의 책 리스트
      **/
-    
+
     @Operation(summary = "등록된 책 리스트", description = "등록된 책 리스트 API 입니다.")
     @GetMapping("/book")
     public ApiResponseHandler<Page<UserBookListDto>> getUserBooks(HttpServletRequest request,
@@ -41,13 +48,15 @@ public class BookController extends BaseController{
                                                                   @RequestParam(required = false) String readStatus ,
                                                                   @RequestParam(required = false) String sortBy,
                                                                   @RequestParam(required = false) String collectionType) {
-        log.info("user 메인 페이지 입니다.");
-        log.info("탭:{}",readStatus);
+        /*log.info("user 메인 페이지 입니다.");*/
+        /*log.info("탭:{}",readStatus);*/
 
         //토큰에서 userNo추출
         Integer userNo = (Integer) request.getAttribute("userNo");
+
         if(userNo==null){
-            throw new IllegalStateException("userNo가 요청에서 누락되었습니다.");
+            log.warn("토큰 없음 또는 유효하지 않음");
+            throw new UnauthenticatedException("로그인 후 이용해주세요.");
         }
         
         //파라미터 sortBy 기준으로 pageable에 정렬 적용
@@ -58,10 +67,11 @@ public class BookController extends BaseController{
         log.info(userBooks.toString());
         return new ApiResponseHandler<>(userBooks, 200, "사용자의 책 리스트를 성공적으로 가져왔습니다.");
     }
+    
     /**
-     * Pageable에 파라미터로 요청한 정렬 조건 반영
-     * @param pageable 기존 페이지 정보(Page 번호, 사이즈, 정렬 등)
-     * @param sortBy 정렬 기준 문자열 ("latestOrder" 또는 "scope")
+     * Pageable에 파라미터로 요청한 정렬을 반영하여 새로운 Pageable 객체 반환
+     * @param pageable 현재 페이지 정보(Page 번호, 크기, 정렬 등)
+     * @param sortBy 정렬 기준 문자열 (예:"registrationDatetime","scope")
      * @return 정렬 조건이 적용된 Pageable 객체
      * */
     private Pageable applySort(Pageable pageable, String sortBy) {
