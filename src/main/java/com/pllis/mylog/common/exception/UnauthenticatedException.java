864990c478b77963dccd585bf92360a7b0eb9390
lkhee <slaqmtm0735@gmail.com>
2025-09-10 17:13:36 +0900

    fix: exception 삭제
    - 삭제이유: 스웨거 에러 이슈

--- src/main/java/com/pllis/mylog/common/exception/UnauthenticatedException.java
+++
@@ -1,9 +0,0 @@
-package com.pllis.mylog.common.exception;
-
-import lombok.extern.slf4j.Slf4j;
-
-public class UnauthenticatedException extends RuntimeException{
-    public UnauthenticatedException(String message) {
-        super(message);
-    }
-}
